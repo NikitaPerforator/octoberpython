d31ad8c73c2dddcb7b234832e9e8584c2b775212
NikitaPerforator <genrytv6713@gmail.com>
2024-10-22 18:23:30 +0300

    Update task_2.py

--- Лабораторная работа 1/task_2.py
+++ Лабораторная работа 1/task_2.py
@@ -1,7 +1,10 @@
-a=4*25*50*100
-mb=a/1024**2
-cout=1
-while mb<=1.44:
-    mb+=mb
-    cout+=1
-print("Количество книг, помещающихся на дискету:",cout)
+# TODO Найдите количество книг, которое можно разместить на дискете
+disk_size_mb = 1.44
+pages_per_book = 100
+lines_per_page = 50
+symbols_per_line = 25
+symbol_size = 4
+disk_size_bytes = disk_size_mb * 1024 * 1024
+book_size_bytes = pages_per_book * lines_per_page * symbols_per_line * symbol_size
+books_count = int(disk_size_bytes / book_size_bytes)
+print("Количество книг, помещающихся на дискету:", books_count)
